0c9139c51260fdd82c8c52d78dd33b564df94f6f
skyrising <simon.wanner@gmx.net>
2014-04-10 18:01:58 +0200

    Added exceptions

--- api.php
+++ api.php
@@ -4,6 +4,10 @@ $resp = new Response();
 header('Content-Type: application/json; charset=UTF-8', true, $resp->http_status);
 echo json_encode($resp);
 
+class NotFoundException extends Exception {}
+class NotAuthorizedException extends Exception {}
+class WrongAuthorizationException extends Exception {}
+
 class Response {
 	public $http_status = 200;
 	public $success = true;
@@ -19,6 +23,12 @@ class Response {
 		}catch(Exception $e) {
 			$this->success = false;
 			$this->http_status = 400;
+			if($e instanceof NotFoundException)
+				$this->http_status = 404;
+			if($e instanceof NotAuthorizedException)
+				$this->http_status = 401;
+			if($e instanceof WrongAuthorizationException)
+				$this->http_status = 403;
 		}
 	}
 }
