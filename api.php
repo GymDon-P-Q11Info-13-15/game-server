1fdcbe7ed13900a4c299421257ad4c5608655092
skyrising <simon.wanner@gmx.net>
2014-04-10 17:59:49 +0200

    Basics

--- api.php
+++ api.php
@@ -1,6 +1,32 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json; charset=UTF-8');
+$resp = new Response();
+header('Content-Type: application/json; charset=UTF-8', true, $resp->http_status);
+echo json_encode($resp);
 
-echo json_encode(null);
+class Response {
+	public $http_status = 200;
+	public $success = true;
+	public $servertime;
+	public $serve_time;
+	
+	function __construct() {
+		try{
+		$this->success = true;
+		$start = microtime(true);
+		$this->servertime = time();
+		$this->serve_time = microtime(true) - $start;
+		}catch(Exception $e) {
+			$this->success = false;
+			$this->http_status = 400;
+		}
+	}
+}
+
+class Result {
+	
+	function get() {
+		return $this;
+	}
+}
 ?>
