47ae8e507211ed8fd873b969ff344c5303257ba4
alcros <alexander.krabler@gmx.de>
2014-05-08 10:32:51 +0200

    Moved Client methods to Remote

--- Game Server/src/de/gymdon/inf1315/game/packet/Remote.java
+++ Game Server/src/de/gymdon/inf1315/game/packet/Remote.java
@@ -2,14 +2,61 @@ package de.gymdon.inf1315.game.packet;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
-public interface Remote {
-    public Socket getSocket();
-    public DataOutputStream getOutputStream();
-    public DataInputStream getInputStream();
-    public boolean left();
-    public void leave(String message);
-    public boolean isServer();
-    public boolean isClient();
+public abstract class Remote {
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private long lastPacketSend;
+    private boolean left = false;
+
+    public Remote(Socket s) throws IOException {
+	this.socket = s;
+	out = new DataOutputStream(socket.getOutputStream());
+	in = new DataInputStream(socket.getInputStream());
+    }
+
+    public Socket getSocket() {
+	if (left() && !socket.isClosed())
+	    try {
+		socket.close();
+	    } catch (IOException e) {
+	    }
+	return socket;
+    }
+
+    public DataOutputStream getOutputStream() {
+	if (left())
+	    throw new RuntimeException("Client left");
+	return out;
+    }
+
+    public DataInputStream getInputStream() {
+	if (left())
+	    throw new RuntimeException("Client left");
+	return in;
+    }
+
+    public boolean left() {
+	return left;
+    }
+
+    public void leave(String message) {
+	if (left)
+	    return;
+	left = true;
+	System.out.println(socket.getInetAddress().getCanonicalHostName()
+		+ " left" + (message != null ? ": " + message : ""));
+	try {
+	    socket.close();
+	} catch (IOException e) {
+	}
+    }
+
+    public abstract boolean isServer();
+
+    public abstract boolean isClient();
 }
--- Game Server/src/de/gymdon/inf1315/game/server/Client.java
+++ Game Server/src/de/gymdon/inf1315/game/server/Client.java
@@ -7,43 +7,15 @@ import java.net.Socket;
 
 import de.gymdon.inf1315.game.packet.Remote;
 
-public class Client implements Remote{
+public class Client extends Remote{
 
-    private Socket socket;
-    private DataOutputStream out;
-    private DataInputStream in;
     private Game game;
     private boolean left = false;
 
     public Client(Socket s) throws IOException {
-	this.socket = s;
-	out = new DataOutputStream(socket.getOutputStream());
-	in = new DataInputStream(socket.getInputStream());
+	super(s);
     }
-
-    @Override
-    public Socket getSocket() {
-	if(left && !socket.isClosed())
-	    try {
-		socket.close();
-	    } catch (IOException e) {
-	    }
-	return socket;
-    }
-
-    @Override
-    public DataOutputStream getOutputStream() {
-	if(left)
-	    throw new RuntimeException("Client left");
-	return out;
-    }
-
-    @Override
-    public DataInputStream getInputStream() {
-	if(left)
-	    throw new RuntimeException("Client left");
-	return in;
-    }
+    
 
     public Client getOtherClient() {
 	if (game == null)
@@ -59,24 +31,10 @@ public class Client implements Remote{
     }
 
     @Override
-    public boolean left() {
-	return left;
-    }
-
-    @Override
     public void leave(String message) {
-	if(left)
-	    return;
+	super.leave(message);
 	if (game != null)
 	    game.end(this);
-	left = true;
-	System.out.println(socket.getInetAddress().getCanonicalHostName()
-		+ " left" + (message != null ? ": " + message : ""));
-	try {
-	    socket.close();
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
     }
 
     @Override
