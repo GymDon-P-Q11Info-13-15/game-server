bf82d597a38da27169f862e93ae8b290084b8606
skyrising <skyrising@pvpctutorials.de>
2014-05-13 21:56:02 +0200

    Why did I not commit these?

--- Game Server/src/de/gymdon/inf1315/game/packet/Packet.java
+++ Game Server/src/de/gymdon/inf1315/game/packet/Packet.java
@@ -6,8 +6,10 @@ import java.util.Map;
 
 public abstract class Packet {
 
+    public static final short ID = -1;
     public static final Map<Short, Class<? extends Packet>> packetTypes = new HashMap<Short, Class<? extends Packet>>();
-
+    public static final int PROTOCOL_VERSION = 1;
+    
     protected Remote remote;
 
     public Packet(Remote r) {
@@ -19,11 +21,9 @@ public abstract class Packet {
     }
 
     public void send() throws IOException {
+	remote.getOutputStream().flush();
 	remote.notifyPacket();
-	remote.getOutputStream().writeShort(getId());
     }
-
-    public abstract short getId();
 
     public static Packet newPacket(short id, Remote r) {
 	if (packetTypes.containsKey(id))
@@ -35,10 +35,24 @@ public abstract class Packet {
 	    }
 	return null;
     }
+    
+    private static void register(Class<? extends Packet> packet) {
+	try {
+	    short id = packet.getField("ID").getShort(null);
+	    if(id < 0)
+		System.err.println("Packet ID undefined: " + packet.getSimpleName());
+	    else if(packetTypes.containsKey(id)) 
+		System.err.println("Duplicate Packet ID: " + packet.getSimpleName() + " (0x" + Integer.toHexString(id) + " = " + packetTypes.get(id).getSimpleName() + ")");
+	    else
+		packetTypes.put(id, packet);
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+    }
 
     static {
-	packetTypes.put(new PacketHello(null).getId(), PacketHello.class);
-	packetTypes.put(new PacketHeartbeat(null).getId(),
-		PacketHeartbeat.class);
+	register(PacketHello.class);
+	register(PacketHeartbeat.class);
+	register(PacketKick.class);
     }
 }
--- Game Server/src/de/gymdon/inf1315/game/server/Server.java
+++ Game Server/src/de/gymdon/inf1315/game/server/Server.java
@@ -5,18 +5,19 @@ import java.util.*;
 import java.io.*;
 import java.util.ArrayList;
 
+import de.gymdon.inf1315.game.Translation;
 import de.gymdon.inf1315.game.packet.Remote;
 
 public class Server implements Runnable {
 
-    public static final int PORT = 22422;
-    public static final String NAME = "Main Server";
     public static Server instance;
 
     List<Remote> clientList = new ArrayList<Remote>();
     private boolean running = false;
     private Timer timer;
     private ConnectionHandler connectionHandler;
+    public Preferences preferences;
+    public Translation translation;
 
     public static void main(String[] args) {
 	new Server();
@@ -26,17 +27,44 @@ public class Server implements Runnable {
 	if (instance != null)
 	    throw new RuntimeException("Already running");
 	instance = this;
+	translation = new Translation("en");
 	this.connectionHandler = new ConnectionHandler(this);
 	this.timer = new Timer(connectionHandler);
+	this.readPreferences();
 	this.run();
+    }
+    
+    private void readPreferences() {
+	File f = new File("preferences.json");
+	try {
+	    if (f.exists())
+		preferences = Preferences.readNew(new FileReader(f));
+	    else {
+		preferences = new Preferences();
+		f.createNewFile();
+		preferences.write(new FileWriter(f));
+		System.out.println(translation.translate("file.created", "preferences.json"));
+	    }
+	    if(preferences.version != Preferences.CURRENT_VERSION) {
+		preferences.version = Preferences.CURRENT_VERSION;
+		preferences = new Preferences();
+		f.createNewFile();
+		preferences.write(new FileWriter(f));
+		System.out.println(translation.translate("updated.version","preferences.json", preferences.version));
+	    }
+	} catch (IOException e) {
+	    throw new RuntimeException("Preferences couldn't be loaded/saved", e);
+	}
     }
 
     public void run() {
 	running = true;
 	ServerSocket ss;
 	try {
-	    ss = new ServerSocket(PORT);
-	    System.out.println("Started server on port " + PORT);
+	    ss = new ServerSocket(preferences.port);
+	    if(!(preferences.hostname.equals("0.0.0.0") || preferences.hostname.equals("*") || preferences.hostname.equals("")))
+		ss.bind(new InetSocketAddress(preferences.hostname, preferences.port));
+	    System.out.println(translation.translate("server.started", preferences.hostname, preferences.port));
 	} catch (IOException e) {
 	    e.printStackTrace();
 	    return;
@@ -45,9 +73,11 @@ public class Server implements Runnable {
 	while (running) {
 	    try {
 		Socket s = ss.accept();
-		clientList.add(new Client(s));
-		System.out.println("New client: "
-			+ s.getInetAddress().getCanonicalHostName());
+		Client c = new Client(s);
+		c.properties.put("translation", translation);
+		clientList.add(c);
+		System.out.println(translation.translate("client.new", s.getInetAddress().getCanonicalHostName()));
+		System.out.flush();
 	    } catch (IOException e) {
 		e.printStackTrace();
 	    }
@@ -66,9 +96,5 @@ public class Server implements Runnable {
 
     public void stop() {
 	running = false;
-    }
-
-    public String getName() {
-	return NAME;
     }
 }
